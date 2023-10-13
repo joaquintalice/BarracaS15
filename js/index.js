5a32880bbfa515e9613d9540121ac987023f9d6f
Joaquin Talice <joaquintalice@gmail.com>
2023-10-12 21:13:26 -0300

    style: product template

--- js/index.js
+++ js/index.js
@@ -29,7 +29,7 @@ function showData(data) {
     for (let prod of data) {
         const { id, name, stock, description, currency, cost } = prod
         template += `
-        <div class='col-4'>
+        <div class='col-12 col-sm-6 col-md-4 col-lg-3'>
             <div class="card">
                 <div class="card-body">
                     <h5 class="card-title">${name}</h5>
